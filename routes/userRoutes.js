965e84b28b07d9cb27e9689ce2172290275f6eea
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-01-23 20:35:40 +0100

    more refactoring

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,37 +1,13 @@
 const express = require('express');
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('./../controllers/userController');
 
 const router = express.Router();
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined',
-  });
-};
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined',
-  });
-};
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined',
-  });
-};
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined',
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined',
-  });
-};
 
 router.route('/').get(getAllUsers).post(createUser);
 
