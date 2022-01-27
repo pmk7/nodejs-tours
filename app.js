795fed4481d03ed04ba15dbff8bf3046bb8b5e1e
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-01-27 11:56:53 +0100

    env variables

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { rawListeners } = require('process');
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
@@ -8,8 +7,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) middleware
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋🏻');
